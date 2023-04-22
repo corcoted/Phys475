4b4b5743a82e3e79f431154109352a7478cb9b0a
Ted Corcovilos <corcovilost@duq.edu>
2023-04-22 18:04:08 -0400

    WIP variational solution

--- TISE-Numerov-var.py
+++ TISE-Numerov-var.py
@@ -43,15 +43,103 @@ autoscale = False # set this equal to true to use Pillai's recommended step size
 # values of constants
 hbar = 1.0
 mass = 1.0 # changing the mass will also change the energy scale
-halfwidth = 1.0 # half width of the well
-V0 = 18.0*np.sqrt(hbar/mass)/halfwidth**2
+a = 1.0 # half width of the well (called a in the homework)
+V0 = 18.0*np.sqrt(hbar/mass)/a**2
+
+
+# %% [markdown]
+# ## Exact solution
+# Following McIntyre section 5.5, the exact energy must be found by solving a transcendental equation. We first define the dimensionless parameter
+#
+# $$
+# z_0 = \sqrt{\frac{2mV_0a^2}{\hbar^2}}.
+# $$
+#
+# Then, we solve the equations
+# $$
+# z \tan z = \sqrt{z_0^2 -z^2}\qquad\text{and}\qquad -z \cot z = \sqrt{z_0^2 -z^2},
+# $$
+# for all of the possible values of $z$.
+
+# %% [markdown]
+# For this problem, we only need the ground state, which will be given by the lowest $z$ value of the tangent equation.
+
+# %%
+z0 = np.sqrt(2*mass*V0)*a/hbar
+print(z0)
+
+# %%
+#solve for z.  We know that the first solution must be in the range [0,π/2].
+z_solution = opt.root_scalar(lambda z: z*np.tan(z)-np.sqrt(z0**2-z**2), bracket=(0,np.pi/2))
+
+z=z_solution.root
+print(z)
+
+# %% [markdown]
+# Now that we know $z$, we can calculate energy and the exact wavefunction.
+#
+# $$
+# z = \sqrt{\frac{2mEa^2}{\hbar^2}} \qquad \rightarrow \qquad E=\frac{\hbar^2}{2ma^2}\,z^2.
+# $$
+
+# %%
+E_exact = (z*hbar/a)**2/2/mass
+print(E_exact)
+
+# %% [markdown]
+# We need four parameters for the wavefunction:
+# $$
+# k = \frac{z}{a},\qquad q=\frac{\sqrt{z_0^2-z^2}}{a}. %k^2+q^2 = (z_0/a)^2
+# $$
+# Putting off normalization:
+# $$ D = 1,\qquad A=e^{qa}\cos(ka) =e^{\sqrt{z_0^2-z^2}} \cos z $$
+
+# %%
+k_exact = z/a
+q_exact = np.sqrt(z0**2-z**2)/a
+D_exact = 1.
+A_exact = np.exp(np.sqrt(z0**2-z**2))*np.cos(z)
+print(f"k={k_exact:.4f}, q={q_exact:.4f}, D={D_exact:.4f}, A={A_exact:.4f}")
+
+
+# %% [markdown]
+# For the sake of completeness, let's calculate the normalization.  By hand...
+# $$
+# \frac{1}{N^2}=\int\limits_{-\infty}^{-a} \left|A\,e^{qx}\right|^2\,dx + 
+# \int\limits_{-a}^{a} \left|D \cos(kx) \right|^2\,dx +
+# \int\limits_{a}^{\infty} \left|A\,e^{-qx}\right|^2\,dx
+# $$
+# Using the even symmetry of the wavefunction, this simplifies a bit:
+# $$
+# \begin{aligned}
+# \frac{1}{2N^2} &= \int\limits_{0}^{a} \left|D \cos(kx) \right|^2\,dx +
+# \int\limits_{a}^{\infty} \left|A\,e^{-qx}\right|^2\,dx\\
+# &= D^2  \left(\frac{a}{2} + \frac12 \frac{\sin(2ka)}{2k}\right)+\frac{A^2}{2q}e^{-2qa}
+# \end{aligned}
+# $$
+
+# %%
+def psi_exact(x):
+    # This is a long way to write the function, but it is clearer than the standard ways
+    return ((x<-1) * (A_exact*np.exp(q_exact*x))+
+           ((x>=-1) * (x<=1)) * (D_exact*np.cos(k_exact*x))+
+           (x>1) * (A_exact*np.exp(-q_exact*x)))
 
 
 # %%
+plt.plot(np.linspace(-3,3,101),psi_exact(np.linspace(-3,3,101)))
+
+# %% [markdown]
+# ## Numerov method
+# The second solution is a discrete position basis matrix approximation using Numerov's approximation for the kinetic energy.  (See paper cited above.)  After calculating the approximate Hamiltonian as a matrix, the approximate eigenstates and energies are found by diagonalizing the Hamiltonian.
+#
+# We'll also reuse the Hamiltonian from the Numerov method to calculate our variational energies in the next section.
+
+# %%
 # bounds (These are overwritten if autoscale=True)
-xmin = -10.*halfwidth # lower bound of position
-xmax = 10*halfwidth # upper bound of position
-n = 1000 # number of steps (may be overwritten if autoscale == True)
+xmin = -5.*a # lower bound of position
+xmax = 5*a # upper bound of position
+n = 200 # number of steps (may be overwritten if autoscale == True)
 dx = (xmax-xmin)/(n-1)
 
 
@@ -62,12 +150,13 @@ def V(x):
     # this also needs to be a "vectorizable" function
     # uncomment one of the examples below, or write your own.
     #return 0.5*mass*omega**2*x*x # harmonic oscillator
-    return np.piecewise(x, [np.abs(x)<= halfwidth, np.abs(x)>halfwidth],[0.0,V0])
+    return np.piecewise(x, [np.abs(x)<= a, np.abs(x)>a],[0.0,V0])
     
 
 
 # %%
 if (autoscale): 
+    # set autoscale=True to use the scalings recommended by Pillai et al.
     #Emax is the maximum energy for which to check for eigenvalues
     Emax = 20.0
     #The next lines make some reasonable choices for the position grid size and spacing
@@ -102,21 +191,39 @@ H = KE + np.diag(V(x))
 #Calculate eigenvalues and eigenvectors of H
 energies, wavefunctions = np.linalg.eigh(H) # "wavefunctions" is a matrix with one eigenvector in each column.
 
+# %% [markdown]
+# For comparison, the eigenvalue calculation took 3.3s for _n_ = 500.
+
 # %%
 true_gs_energy = energies[0]
 
 
+# %% [markdown]
+# ## Variational approximation
+# Here we reuse the Hamiltonian matrix from above, but we use it to calculate the approximate energy expectation values for our trial wavefunctions.  Then we adjust the trial wavefunctions to minimize the energy expectation value.
+
+# %%
+# rectangle function for making it easier to write our trial wavefunctions
+def rectangle(x,a):
+    '''rectangle is one for -a<x<a and zero otherwise'''
+    return np.heaviside(x+a,0)-np.heaviside(x-a,0)
+
+
 # %%
 #trial wavefunction
-def psi(a=1.0):
+def psi(b=1.0):
     '''Wavefunction as a function of position x.
-    The array "a" stores any parameters.'''
-    # Note, normalization is not necessary
-    #psi_unnorm = np.exp(-x**2*a) # gaussian
-    #psi_unnorm = np.piecewise(x,[np.abs(x*a)<1.0,np.abs(x*a)>=1.0],[lambda x:1.0+np.cos(a*np.pi*x),0.0])# cosine**2 piece
-    psi_unnorm = np.piecewise(x,[np.abs(x*a)<1.0,np.abs(x*a)>=1.0],[lambda x:np.cos(a*np.pi*x/2.0),0.0])# cosine piece
+    The variable b is the parameter that we tune to minimize the energy.'''
+    # Note, normalization is calculated below.
+    psi_unnorm = np.exp(-x**2*b) # gaussian
+    #psi_unnorm = np.cos(b*np.pi*x/2.0)*rectangle(x,1.0/b)# cosine piece
+    #psi_unnorm = np.cos(b*np.pi*x/2.0)**2*rectangle(x,1.0/b)# cosine-squared piece
     norm = 1.0/np.sqrt(np.dot(psi_unnorm,psi_unnorm))
     return norm*psi_unnorm
+
+#limits on b (to help the minimization function)
+bmin = 0.1
+bmax = 10.
 
 
 # %%
@@ -126,17 +233,20 @@ def ev(psi):
 
 
 # %%
-# minimize expectation value with respect to a
-result=opt.minimize_scalar(lambda a: ev(psi(a)))
+# minimize expectation value with respect to b
+result=opt.minimize_scalar(lambda b: ev(psi(b)),(bmin,bmax))
 
+
+# %% [markdown]
+# For comparison, the variational calculation took 0.3s with _n_=500.
 
 # %%
-print(f"The value of a is {result.x}, with an energy of {result.fun}, compared to the true gs energy of {true_gs_energy}")
+print(f"The value of b is {result.x:.4f}, with an energy of {result.fun:.4f}, compared to the energy from the Numerov diagonalization of {true_gs_energy:.4f}.")
 
 # %%
 zoom = 3.0 # zoom factor for wavefunctions to make them more visible
 plt.plot(x,V(x),'-k',label="V(x)") # plot the potential
-plt.plot(x,zoom*np.sign(wavefunctions[n//2,0])*wavefunctions[:,0]+energies[0],label="true") #plot the num-th wavefunction
+plt.plot(x,zoom*np.sign(wavefunctions[n//2,0])*wavefunctions[:,0]+energies[0],label="Numerov") #plot the num-th wavefunction
 plt.plot(x,zoom*psi(result.x)+result.fun,label="variational") # plot the variational wavefunction
 plt.ylim(-1,3); # set limits of vertical axis for plot
 plt.xlim(-3,3); # set limits of horizontal axis for plot
